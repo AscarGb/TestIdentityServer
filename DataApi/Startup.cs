ba2c6838f35285d79069509ba60f2b23271fa1fe
AscarGb <ascargb>
2018-10-13 15:08:36 +0500

    1

---
+++ DataApi/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Types;
+
+namespace DataApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            var section = Configuration.GetSection("ApiConfiguration");
+            services.Configure<ApiConfiguration>(section);
+
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+
+
+            ApiConfiguration settings = section.Get<ApiConfiguration>();
+
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = settings.Authority;
+                    options.RequireHttpsMetadata = false;
+                    options.ApiName = settings.ApiName;
+                });
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
---
+++ Types/ApiConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Types
+{
+    public class ApiConfiguration
+    {
+        public string ApiName { get; set; }
+        public string Authority { get; set; }
+    }
+}
